5124078596c7da58bb5170675d0dc0c59822a21b
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-03-08 16:55:17 +0100

    Inserite arrow in Index. Inserito ciclo For in JS.

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const imagesArray = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp',
+];
+
+// Array
+
+const itemDOMElement = document.querySelector(".item-container"); 
+
+// Creo una costante e le do un nome "parlante" per collegarla alla classe "item-container" della pagina HTML.
+
+for (let index = 0; index < imagesArray.length; index ++ ) {
+    console.log(imagesArray[index]);
+
+    // Con questo ciclo "for" sto creando una variabile, "index", e la incremento di uno finché il suo contenuto sia inferiore all'Array contenente le immagini. Subito dopo eseguo un "console.log" per verificare che la console del browser mi restiuisca, come poi avviene, la index delle immagini dell'Array. 
+    
+    const imgSrc = imagesArray [index];
+    // Creo una costante e le do nome "imgSrc": coincide con "imagesArray [index]"". 
+
+    const slideItem = `
+    <div class= "item">
+        <img src= "${imgSrc}">
+    </div>
+    `;
+
+    console.log(slideItem);
+    itemDOMElement.innerHTML += slideItem;
+
+}
+
