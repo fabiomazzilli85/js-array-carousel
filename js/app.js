2358e60a19823d7af3a51794ac813d51b6ef686c
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-03-09 13:35:11 +0100

    Rimossi blocchi di codice inutilizzati.

--- js/app.js
+++ js/app.js
@@ -9,8 +9,10 @@ const imagesArray = [
 // Array
 
 
-let ciao
 const itemDOMElement = document.querySelector(".item-container"); 
+
+// Creo una costante e le do il nome "itemDOMElement e la collego a "item-container" all'interno della index.
+
 
 for (let index = 0; index < imagesArray.length; index ++ ) {
     console.log(imagesArray[index]);
@@ -27,19 +29,10 @@ for (let index = 0; index < imagesArray.length; index ++ ) {
     itemDOMElement.innerHTML += slideItem;
 }
 
-// Ciclo For
+// Questo è un ciclo For. Creo una variabile "index" e la incremento di 1 affinché la condizione che la "index" sia minore del numero degli elementi all'interno dell'Array sia vera. Quando la condizione non è più vera, il ciclo For si interrompe.
 
 
 const arrowElement = document.querySelector('.arrow');
 
-document.getElementById("left").addEventListener("click", function (){
-let precedente =  document.getElementsByClassName("item");
-});
+// Creo la costante e la associo alla classe "arrow" all'interno della index per richiamarla.
 
-document.getElementById("right").addEventListener("click", function (){
-let successiva = document.getElementsByClassName("item-active");
-});
-
-
-// arrowElement.classList.add("bg-brown");
-// Ho aggiunto una classe con alcune prorietà CSS con JS.
