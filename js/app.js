f93c4f90c77b1c3c70a1fb898de01200a24f2dad
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-03-09 15:08:11 +0100

    Create funzione per rimuovere classe CSS.

--- js/app.js
+++ js/app.js
@@ -5,34 +5,36 @@ const imagesArray = [
     'img/04.webp',
     'img/05.webp',
 ];
-
-// Array
+// Creo Array contente cinque elementi: si parte da 0 e si arriva a 4.
 
 
 const itemDOMElement = document.querySelector(".item-container"); 
-
-// Creo una costante e le do il nome "itemDOMElement e la collego a "item-container" all'interno della index.
+// Creo una costante e le do il nome "itemDOMElement e la collego alla classe "item-container".
 
 
 for (let index = 0; index < imagesArray.length; index ++ ) {
     console.log(imagesArray[index]);
-
+    
     const imgSrc = imagesArray [index];
-
+    
     const slideItem = `
     <div class= "item">
-        <img src= "${imgSrc}">
+    <img src= "${imgSrc}">
     </div>
     `;
-
+    
     console.log(slideItem);
     itemDOMElement.innerHTML += slideItem;
 }
+// Questo è un ciclo For. Creo una variabile che chiamo "index" e la incremento di 1 affinché la condizione (index < del numero degli elementi dell'Array) è vera. Quando la condizione non è più vera, il ciclo For si interrompe.
 
-// Questo è un ciclo For. Creo una variabile "index" e la incremento di 1 affinché la condizione che la "index" sia minore del numero degli elementi all'interno dell'Array sia vera. Quando la condizione non è più vera, il ciclo For si interrompe.
 
+function myFunction() {
+    let element = document.getElementById("left");
+    element.classList.remove("item-active");
+}
 
-const arrowElement = document.querySelector('.arrow');
-
-// Creo la costante e la associo alla classe "arrow" all'interno della index per richiamarla.
-
+function myFunctionTwo() {
+    let element = document.getElementById("right");
+    element.classList.remove("item-active");
+    
